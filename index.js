6b2a9be427ba5fb66ca59166d212e501e13f856c
probably-not-porter <pelibby16@earlham.edu>
2019-05-10 20:33:37 -0400

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+var path = require('path');
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+    res.render("index",{
+    })
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, '192.168.43.48', function() {
+    console.log('Listening to port:  ' + 3000);
+});
---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+// Internal logic section
