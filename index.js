80ca587c73b358060195a69ff3df7916be3da309
probably-not-porter <pelibby16@earlham.edu>
2019-05-12 01:30:39 -0400

    update top level

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get('/', function(req, res) {
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function() {
-    console.log('Listening to port:  ' + 3000);
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
